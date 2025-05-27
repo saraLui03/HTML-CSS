53647e5aeddcce34657a75a92221252723cb603a
SaraL <lui03996@gmail.com>
2025-05-27 21:00:07 +0200

    Cambios para el ejercicio PEC3_HTML_CSS

--- module.js
+++ module.js
@@ -5,17 +5,20 @@ import FresaImg from "url:./src/Img/Escritorio/choclolateBlancoConFresas.webp"
 
 
 const images = [
-    
+
     mangoImg,
-    LogoImg, 
+    LogoImg,
     FresaImg];
-    
-const gridCells = document.querySelectorAll(".element img");
+
+
+let gridCells = document.querySelectorAll(".element img");
+
 
 function changeRandomImage() {
+
     const randomCell = Math.floor(Math.random() * gridCells.length);
     let randomImage = images[Math.floor(Math.random() * images.length)];
-    while(randomImage == gridCells[randomCell].src) {
+    while (randomImage == gridCells[randomCell].src) {
         randomImage = images[Math.floor(Math.random() * images.length)];
     }
 
@@ -28,7 +31,31 @@ function changeRandomImage() {
     }, 500); // Tiempo de transición
 }
 
-// Alternar imágenes cada 2 segundos
-setInterval(changeRandomImage, 2000);
-export{changeRandomImage}
 
+
+
+function waitForElement(selector, callback) {
+    const interval = setInterval(() => {
+        const element = document.querySelector(selector);
+
+        if (element) {
+            clearInterval(interval);
+            callback(element);
+        }
+    }, 100); // Check every 100ms
+}
+
+// Usage
+waitForElement('.element img', (element) => {
+   gridCells = document.querySelectorAll(".element img");
+   if (gridCells.length > 0) {
+        
+    changeRandomImage()
+    // Alternar imágenes cada 2 segundos
+        setInterval(changeRandomImage, 2000);
+
+    }
+    // Perform actions on the element
+});
+
+export { changeRandomImage }
